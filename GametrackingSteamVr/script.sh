d8770d25d1d9beb100e331965875e25a5fa8ebc4
Antoine Rybacki <15911822+Lifeismana@users.noreply.github.com>
2025-05-04 01:04:28 +0200

    manifest

--- GametrackingSteamVr/script.sh
+++ GametrackingSteamVr/script.sh
@@ -28,11 +28,11 @@ shopt -s extglob
 for file in ./manifests/*_*_*.txt; do
     mv $file ${file%_+([0-9]).txt}.txt
 done
-/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -all-platforms -depot 250821 -validate -dir . -beta $STEAM_BRANCH -filelist "/data/list/dll.txt"
-/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -all-platforms -depot 250823 250832 -validate -dir . -beta $STEAM_BRANCH -filelist "/data/list/lib.txt"
-/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -all-platforms -depot 250824 -validate -dir . -beta $STEAM_BRANCH -filelist "/data/list/big_content.txt"
-/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -all-platforms -depot 250827 250830 250833 -validate -dir . -beta $STEAM_BRANCH -filelist "/data/list/content.txt"
-/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -all-platforms -depot 250834 -validate -dir . -beta $STEAM_BRANCH 
+/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -all-platforms -depot 250821 -validate -dir . -beta $STEAM_BRANCH -manifest 1606522145430770202  -filelist "/data/list/dll.txt"
+/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -all-platforms -depot 250823 250832 -validate -dir . -beta $STEAM_BRANCH -manifest 7064729533953426393 710963398945632302  -filelist "/data/list/lib.txt"
+/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -all-platforms -depot 250824 -validate -dir . -beta $STEAM_BRANCH -manifest 5354332068842395310 -filelist "/data/list/big_content.txt"
+/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -all-platforms -depot 250827 250830 250833 -validate -dir . -beta $STEAM_BRANCH -manifest 2190029673101935499 7391958373704501609 8084172187417437723 -filelist "/data/list/content.txt"
+/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -all-platforms -depot 250834 -validate -dir . -manifest 3239572551071072677 -beta $STEAM_BRANCH 
 /data/DepotDownloader/DepotDownloader -app 250820 -all-platforms -depot 250825 -dir . -beta $STEAM_BRANCH
 npm install
 
