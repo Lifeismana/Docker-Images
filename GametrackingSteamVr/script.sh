4e3dab12fc61d32f4d212ff08ac406ece7459110
Antoine Rybacki <15911822+Lifeismana@users.noreply.github.com>
2025-05-04 10:54:39 +0200

    fix manifest

--- GametrackingSteamVr/script.sh
+++ GametrackingSteamVr/script.sh
@@ -22,8 +22,10 @@ mkdir -p ./manifests
 
 [ -z "$STEAM_BRANCH" ] && STEAM_BRANCH="beta"
 
+/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -depot 228985 228988 228989 228990 -all-platforms -dir ./manifests -manifest-only
 # 817940
-/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -depot 228985 228988 228989 228990 250821 250822 250823 250824 250825 250827 250828 250829 250830 250831 250832 250833 250834 -all-platforms -dir ./manifests -beta $STEAM_BRANCH -manifest-only
+/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -depot 250822 250825 250828 250829 250831 -all-platforms -dir ./manifests -beta $STEAM_BRANCH -manifest-only
+/data/DepotDownloader/DepotDownloader -username "$STEAM_USERNAME" -password "$STEAM_PASSWORD" -app 250820 -depot 250821 250823 250824 250827 250830 250832 250833 250834 -manifest 1606522145430770202 7064729533953426393 5354332068842395310 2190029673101935499 7391958373704501609 710963398945632302 8084172187417437723 3239572551071072677 -all-platforms -dir ./manifests -beta $STEAM_BRANCH -manifest-only
 shopt -s extglob
 for file in ./manifests/*_*_*.txt; do
     mv $file ${file%_+([0-9]).txt}.txt
